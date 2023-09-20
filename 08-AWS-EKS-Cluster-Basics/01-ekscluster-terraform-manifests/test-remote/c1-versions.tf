0326e09e47bb688adc3baa122e941c4dce078ae3
truongpx-Mac <truongpx396@gmail.com>
2023-09-20 19:35:12 +0700

    update

--- 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/test-remote/c1-versions.tf
+++ 08-AWS-EKS-Cluster-Basics/01-ekscluster-terraform-manifests/test-remote/c1-versions.tf
@@ -21,19 +21,19 @@ terraform {
     }
   }
   # Adding Backend as S3 for Remote State Storage
-  backend "s3" {
-    bucket = "terraform-on-aws-eks"
-    key    = "dev/ebs-storage/terraform.tfstate"
-    region = "us-east-1" 
+  # backend "s3" {
+  #   bucket = "terraform-on-aws-eks"
+  #   key    = "dev/ebs-storage/terraform.tfstate"
+  #   region = "us-east-1" 
 
-    # For State Locking
-    dynamodb_table = "dev-ebs-storage"    
-  }     
+  #   # For State Locking
+  #   dynamodb_table = "dev-ebs-storage"    
+  # }     
 }
 
 # Terraform AWS Provider Block
 provider "aws" {
-  region = var.aws_region
+  region = "ap-southeast-1"
 }
 
 # Terraform HTTP Provider Block
