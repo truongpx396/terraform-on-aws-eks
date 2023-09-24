47140a8d2cdff2b7b77216d663a5c4d2a6770d31
truongpx-Mac <truongpx396@gmail.com>
2023-09-24 17:23:13 +0700

    update

--- 14-EBS-CSI-Install-Kubernetes-Storage/istio/addons_installer.tf
+++ 14-EBS-CSI-Install-Kubernetes-Storage/istio/addons_installer.tf
@@ -1,12 +1,25 @@
-data "kubectl_filename_list" "manifests" {
-    pattern = "./addons/*"
+data "kubectl_filename_list" "manifests_addons_extras" {
+    pattern = "./addons/extras/*.yaml"
 }
 
-resource "kubectl_manifest" "istio_addons_installer" {
-    count = length(data.kubectl_filename_list.manifests.matches)
-    yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
+resource "kubectl_manifest" "istio_addons_extras_installer" {
+    for_each  = toset(data.kubectl_path_documents.manifests_addons_extras.documents)
+    yaml_body = each.value
 
     depends_on = [
     helm_release.istio,
+  ]
+}
+
+data "kubectl_filename_list" "manifests" {
+    pattern = "./addons/*.yaml"
+}
+
+resource "kubectl_manifest" "istio_addons_installer" {
+    for_each  = toset(data.kubectl_path_documents.manifests.documents)
+    yaml_body = each.value
+
+    depends_on = [
+    kubectl_manifest.istio_addons_extras_installer,
   ]
 }
