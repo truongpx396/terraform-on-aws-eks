4957861aa1ed5617cd2a9a9d075f81f6ef08d7bc
truongpx-Mac <truongpx396@gmail.com>
2023-09-27 17:36:53 +0700

    update

--- 14-EBS-CSI-Install-Kubernetes-Storage/istio-service-mesh/main.tf
+++ 14-EBS-CSI-Install-Kubernetes-Storage/istio-service-mesh/main.tf
@@ -1,9 +1,10 @@
 resource "helm_release" "istio" {
  
   repository          = var.helm_repository
-  name = var.helm_release
-
   chart     = "istiod"
+
+  name = "istio"
+
   version   = var.chart_version
   namespace = var.helm_namespace
   timeout   = 1200
@@ -14,5 +15,25 @@ resource "helm_release" "istio" {
 
   depends_on = [
     kubernetes_namespace.istio_system,
+  ]
+}
+
+resource "helm_release" "istio-ingressgateway" {
+ 
+  repository          = var.helm_repository
+  chart     = "gateway"
+
+  name = "istio-ingressgateway"
+
+  version   = var.chart_version
+  namespace = var.helm_namespace
+  timeout   = 1200
+
+  # values = [
+  #   var.values,
+  # ]
+
+  depends_on = [
+    helm_release.istio,
   ]
 }
