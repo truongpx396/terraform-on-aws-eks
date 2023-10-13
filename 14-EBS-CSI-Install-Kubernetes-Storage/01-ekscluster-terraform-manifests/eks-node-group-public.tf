0f4b41e5f14277cd58817520cfbdf095d766f116
truongpx-Mac <truongpx396@gmail.com>
2023-10-13 17:28:19 +0700

    update

--- 14-EBS-CSI-Install-Kubernetes-Storage/01-ekscluster-terraform-manifests/eks-node-group-public.tf
+++ 14-EBS-CSI-Install-Kubernetes-Storage/01-ekscluster-terraform-manifests/eks-node-group-public.tf
@@ -23,9 +23,9 @@ resource "aws_eks_node_group" "eks_ng_public" {
   # }
 
   scaling_config {
-    desired_size = 2
-    min_size     = 1    
-    max_size     = 2
+    desired_size = 3
+    min_size     = 2    
+    max_size     = 4
   }
 
   # Desired max percentage of unavailable worker nodes during node group update.
