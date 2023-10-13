3afa3266b63c81755cf21c2cf58b6205301d8548
truongpx-Mac <truongpx396@gmail.com>
2023-10-13 17:53:01 +0700

    update

--- 14-EBS-CSI-Install-Kubernetes-Storage/03-externaldns-install-terraform-manifests/c4-03-externaldns-install.tf
+++ 14-EBS-CSI-Install-Kubernetes-Storage/03-externaldns-install-terraform-manifests/c4-03-externaldns-install.tf
@@ -8,10 +8,10 @@ resource "helm_release" "external_dns" {
 
   namespace = "kube-system"     
 
-  set {
-    name = "image.repository"
-    value = "k8s.gcr.io/external-dns/external-dns" 
-  }       
+  # set {
+  #   name = "image.repository"
+  #   value = "k8s.gcr.io/external-dns/external-dns" 
+  # }       
 
   set {
     name  = "serviceAccount.create"
